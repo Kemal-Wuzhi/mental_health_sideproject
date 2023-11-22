b6f10482817282774258b8ea64e031689248debd
Kemal-Wuzhi <camelwuzhi@gmail.com>
2023-11-23 00:59:53 +0800

    feat:init login feature

---
+++ PaitentPlatform/frontEnd/src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="login-container">
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" v-model="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" v-model="password">
+            </div>
+            <button type="submit">Login</button>
+        </form>
+
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+
+const email = ref('')
+const password = ref('')
+
+// login logic 
+
+</script>
+
+<style></style>
